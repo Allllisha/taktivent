e4fc5d231b14ec4cb7e7687dd3d7c3a29cda5ede
Tom Tsui <tomtsui2642@gmail.com>
2022-02-12 17:55:37 +0900

    SongReviews controller

---
+++ test/controllers/song_reviews_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class SongReviewsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get song_reviews_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get song_reviews_create_url
+    assert_response :success
+  end
+end
