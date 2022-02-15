b033e69c6db41b538b28ade5857ba0f4b5aedd59
Nico L <l.nico08@laposte.net>
2022-02-15 22:05:32 +0900

    now using render for footer in homepage and starting dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :performers, dependent: :destroy
+  has_many :events
 
   validates :first_name, :last_name, presence: true
 end
