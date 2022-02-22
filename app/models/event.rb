96237cd3aa570508dde2efd5d97da0b95af19a84
Tom Tsui <tomtsui2642@gmail.com>
2022-02-22 20:53:34 +0900

    Event has_many song_reviews through songs

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   belongs_to :venue
   has_many :event_reviews, dependent: :destroy
   has_many :songs, dependent: :destroy
+  has_many :song_reviews, through: :songs
   has_many_attached :images
 
   validates :name, :start_at, :end_at, :user, :venue, presence: true
