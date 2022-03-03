71c4a8a6f5ad267df6fc88e369f43eeff2bace9b
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2022-03-03 20:19:59 +0900

    new time trigger

--- app/javascript/controllers/song_info_controller.js
+++ app/javascript/controllers/song_info_controller.js
@@ -1,39 +1,36 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ 'songInfoStart',  'eventInfoStart', 'eventImg', 'songImg', 'eventName', 'songName']
+  static targets =  ['eventInfoStart', 'songInfoStart', 'eventImg']
 
   connect() {
     console.log('Hello from song_info_controller.js')
     // console.log(this.testTarget)
-   
-    this.eventInfoStart = new Date(this.eventInfoStartTarget.dataset.event);
     this.songInfoStart = new Date(this.songInfoStartTarget.dataset.start);
-    this.eventName = this.eventNameTarget.innerText
-    this.songName = this.songInfoStartTarget.dataset.song;
-    console.log(this.eventName)
-    console.log(this.songName )
-    setInterval(this.eventImgContainer.bind(this),1000);
+    // this.eventInfoStart = new Date(this.eventInfoStartTarget.dataset.event);
+    setInterval(this.songImgContainer.bind(this),1000);
+    // setInterval(this.eventImgContainer.bind(this),1000);
   }
 
 
-  eventImgContainer(){
-    console.log(this.eventInfoStart.valueOf());
-    console.log(new Date().valueOf());
-    console.log(new Date().valueOf() >=  this.eventInfoStart.valueOf());
-     if(new Date().valueOf() >=  this.eventInfoStart.valueOf()){
+
+  songImgContainer(){
+    // console.log(this.eventInfoStart.valueOf());
+    // console.log(new Date().valueOf());
+    // console.log(new Date().valueOf() >=  this.eventInfoStart.valueOf());
+     if(new Date().valueOf() >=  this.songInfoStart.valueOf()){
       this.eventImgTarget.classList.add('d-none')
      }
   }
 
+  // eventImgContainer(){
+  //   // console.log(this.eventInfoStart.valueOf());
+  //   // console.log(new Date().valueOf());
+  //   // console.log(new Date().valueOf() >=  this.eventInfoStart.valueOf());
+  //    if(new Date().valueOf() >=  this.eventInfoStart.valueOf()){
+      
+  //    }
+  // }
 
- songImgContainer(){
-  console.log(this.songInfoStart.valueOf());
-  console.log(new Date().valueOf());
-  console.log(new Date().valueOf() >=  this.songInfoStart.valueOf());
-   if(new Date().valueOf() >=  this.songInfoStart.valueOf()){
-    this.songImgTarget.classList.remove('d-none')
-   }
-}
 
 }
