85c5df7e55e62b45d6731e957759d09d8d1dacf4
Tom Tsui <tomtsui2642@gmail.com>
2022-03-03 22:07:03 +0900

    Migrated db so sentiment can be null in EventReview and SongReview

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_113154) do
+ActiveRecord::Schema.define(version: 2022_03_03_130437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_113154) do
     t.bigint "event_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "sentiment", null: false
+    t.integer "sentiment"
     t.jsonb "responses", default: "{}", null: false
     t.index ["event_id"], name: "index_event_reviews_on_event_id"
   end
@@ -91,7 +91,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_113154) do
     t.bigint "song_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "sentiment", null: false
+    t.integer "sentiment"
     t.jsonb "responses", default: "{}", null: false
     t.index ["song_id"], name: "index_song_reviews_on_song_id"
   end
