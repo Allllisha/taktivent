1154a46251df5b8fb5e79c9620966bbe01be2679
Tom Tsui <tomtsui2642@gmail.com>
2022-03-05 14:07:16 +0900

    fixed events/new.html.erb styles

--- app/javascript/controllers/events_new_controller.js
+++ app/javascript/controllers/events_new_controller.js
@@ -1,24 +1,20 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['eventImageInput', 'eventImage', 'eventBackground' ]
+  static targets = [ 'eventImage', 'eventBackground' ]
 
   connect() {
   }
 
   displayUploadedImage() {
     this.eventBackgroundTarget.style.display = "none";
-    // const image = event.target.files[0];
-    // console.log(image.size);
     const reader = new FileReader();
     const eventImage = this.eventImageTarget;
     reader.onload = function (file) {
       const img = new Image();
-      // console.log(file);
       img.src = file.target.result;
       eventImage.appendChild(img);
     }
     reader.readAsDataURL(event.target.files[0]);
-    // console.log(files);
   }
 }
