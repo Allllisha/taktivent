bf2cc40a6dd713bb315d30aeca6aa0eaa81590f1
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2022-03-08 03:10:21 +0900

    refactor

--- app/javascript/controllers/event_info_controller.js
+++ app/javascript/controllers/event_info_controller.js
@@ -1,24 +1,40 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ 'event', 'info', 'eventBox', 'eventAbout' ]
+  static targets = [ 'event', 'info', 'eventBox', 'eventAbout', 'about']
 
   connect() {
     // console.log(this.testTarget)
 
-  //   this.eventStart = new Date(this.eventTarget.dataset.start);
+    this.eventStart = new Date(this.eventTarget.dataset.start);
+    this.about = new Date(this.aboutTarget.dataset.about);
+    console.log(this.about)
   //   this.eventEnd = new Date(this.infoTarget.dataset.end);
   //  //  setInterval(this.showSongCountdown,1000);
-  //   setInterval(this.eventEndContainer.bind(this),1000);
-  //   setInterval(this.eventAboutEndContainer.bind(this),1000);
+   setInterval(this.EventAboutContainer.bind(this),1000);
+    setInterval(this.EventStartContainer.bind(this),1000);
   }
+
+
+ EventStartContainer(){
+    if(new Date().valueOf() >=  this.eventStart.valueOf()){
+     this.eventBoxTarget.classList.add('d-none')
+    }
+  }
+
+  EventAboutContainer(){
+    if(new Date().valueOf() >=  this.about.valueOf()){
+      this.eventAboutTarget.classList.add('d-none')
+     }
+  }
+
 
  eventInfoContainer(event){
   //  console.log(this.eventStart.valueOf());
   //  console.log(new Date().valueOf());
   //  console.log(new Date().valueOf() >=  this.eventStart.valueOf());
-    if(event.key === 'v'){
-     this.eventBoxTarget.classList.add('d-none')
+    if(event.key === 'e'){
+     this.eventBoxTarget.classList.remove('d-none')
     }
  }
 
@@ -36,9 +52,7 @@ export default class extends Controller {
   // console.log(this.eventStart.valueOf());
   // console.log(new Date().valueOf());
   // console.log(new Date().valueOf() >=  this.eventStart.valueOf());
-   if(event.key === 'v'){
-    this.eventAboutTarget.classList.add('d-none')
-   }else if(new Date().valueOf() >=  this.eventEnd.valueOf()){
+   if(event.key === 'e'){
     this.eventAboutTarget.classList.remove('d-none')
    }
 }
