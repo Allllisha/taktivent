866817902b0efa6da36ad3c1897c24ac2b8c2870
Tom Tsui <tomtsui2642@gmail.com>
2022-03-08 23:15:21 +0900

    form in events/new.html displays appropiate number of question fields and choice fields according to user's selection

---
+++ app/javascript/controllers/questions_customization_form_controller.js
@@ -0,0 +1,51 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = ['questionForm', 'choiceField', 'choiceSection' ]
+
+  connect() {
+    // console.log('Hello from questions_customization_form_controller.js')
+    // console.log(this.testTarget)
+  }
+
+  // display appropiate number of questions according to selection
+  displayQuestionForms() {
+    let i = 1;
+    this.questionFormTargets.forEach((element) => {
+      if ( event.target.value >= i) {
+        element.hidden = false;
+      } else {
+        element.hidden = true;
+      }
+      i++;
+    });
+  }
+
+  // display appropiate number of choice entry fields according to selection
+  displayChoiceFields() {
+    let i = 1;
+    this.choiceFieldTargets.forEach((element) => {
+      if (event.target.dataset.questionNumber === element.dataset.questionNumber) {
+        if (event.target.value >= i) {
+          element.hidden = false;
+        } else {
+          element.hidden = true;
+        }
+        i++;
+      }
+    });
+  }
+
+  // hide choice field if the question type is starts
+  // (stars questions choices are always 1-5)
+  hideChoiceSectionForStarsQuestion() {
+    this.choiceSectionTargets.forEach((element) => {
+      if (event.target.dataset.questionNumber === element.dataset.questionNumber &&
+          event.target.value === "stars") {
+        element.hidden = true;
+      } else {
+        element.hidden = false;
+      }
+    });
+  }
+}
