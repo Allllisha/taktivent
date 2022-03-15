547d66f1e3874a28af6ac19eed79d5a91db776b5
Tom Tsui <tomtsui2642@gmail.com>
2022-03-15 13:28:59 +0900

    In question_customization_form_controller.js, replace variable i with index in forEach callback. Also added comments

--- app/javascript/controllers/questions_customization_form_controller.js
+++ app/javascript/controllers/questions_customization_form_controller.js
@@ -3,43 +3,44 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ['questionForm', 'choiceField', 'choiceSection' ]
 
-  connect() {
-    // console.log('Hello from questions_customization_form_controller.js')
-    // console.log(this.testTarget)
-  }
+  // connect() {
+  //   console.log('Hello from questions_customization_form_controller.js')
+  //   console.log(this.testTarget)
+  // }
 
-  // display appropiate number of questions according to selection
+  // display correct number of questions according to user's choice
   displayQuestionForms(event) {
-    let i = 1;
-    this.questionFormTargets.forEach((element) => {
-      if ( event.target.value >= i) {
+    // loop through all question customization input fields
+    // display the input field if its index does not exceed the desired number of questions
+    this.questionFormTargets.forEach((element, index) => {
+      if ( event.target.value >= index + 1) { // starts from 1 instead of 0
         element.hidden = false;
       } else {
         element.hidden = true;
       }
-      i++;
     });
   }
 
-  // display appropiate number of choice entry fields according to selection
+  // display correct number of choice input fields according to user's choice
   displayChoiceFields(event) {
-    let i = 1;
-    this.choiceFieldTargets.forEach((element) => {
+    // loop through all choice input fields of that questions
+    // display the input field if its index does not exceed the desired number of questions
+    this.choiceFieldTargets.forEach((element, index) => {
       if (event.target.dataset.questionNumber === element.dataset.questionNumber) {
-        if (event.target.value >= i) {
+        if (event.target.value >= index + 1) { // starts from 1 instead of 0
           element.hidden = false;
         } else {
           element.hidden = true;
         }
-        i++;
       }
     });
   }
 
-  // hide choice field if the question type is stars
+  // hide choice input field if the question type is stars
   // (stars questions choices are always 1-5)
   hideChoiceSectionForStarsQuestion(event) {
     this.choiceSectionTargets.forEach((element) => {
+      // only display or hide the input field if it and the question type selection field have the same question number
       if (event.target.dataset.questionNumber === element.dataset.questionNumber) {
         if (event.target.value === "stars") {
           element.hidden = true;
